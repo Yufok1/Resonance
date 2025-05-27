a5eb74680dacc5bc9f18b84586aba326b58aff58
Yufok1 <towers.jeff@gmail.com>
2025-05-27 05:47:41 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,3 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCIBZiQV9b3jjdDxZl5HTEGuGo9Qy8Z628",
-  authDomain: "resonancefield.firebaseapp.com",
-  databaseURL: "https://resonancefield-default-rtdb.firebaseio.com",
-  projectId: "resonancefield",
-  storageBucket: "resonancefield.firebasestorage.app",
-  messagingSenderId: "902374308200",
-  appId: "1:902374308200:web:298a6133c1f34c21dd3edd",
-  measurementId: "G-6DVX6SN8CL"
-};
-
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+// Firebase config removed for security.
+// Please create this file locally with your config.
