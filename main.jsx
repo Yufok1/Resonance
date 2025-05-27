44838e5b330d6e6afe25d705dd55aee7fb8d0295
Yufok1 <towers.jeff@gmail.com>
2025-05-27 01:36:06 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ResonanceField from './ResonanceField';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ResonanceField />
+  </React.StrictMode>
+);
