f0cb6ac628fb60abb628c259fcbfaa98f6596c06
Yufok1 <towers.jeff@gmail.com>
2025-05-27 01:39:08 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ResonanceField from './src/ResonanceField';
+import ResonanceField from './src/ResonanceField.jsx';
 import './src/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
