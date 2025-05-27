82e9c9fc65aa2d12f61f788b7cacafd71bfb1ca5
Yufok1 <towers.jeff@gmail.com>
2025-05-27 01:37:34 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ResonanceField from './ResonanceField';
-import './index.css';
+import ResonanceField from './src/ResonanceField';
+import './src/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
