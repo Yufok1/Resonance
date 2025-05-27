be0bd1d34fceb64aba03681aeb581f8f56ba125d
Yufok1 <towers.jeff@gmail.com>
2025-05-27 02:58:27 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCIBZiQV9b3jjdDxZl5HTEGuGo9Qy8Z628",
+  authDomain: "resonancefield.firebaseapp.com",
+  databaseURL: "https://resonancefield-default-rtdb.firebaseio.com",
+  projectId: "resonancefield",
+  storageBucket: "resonancefield.firebasestorage.app",
+  messagingSenderId: "902374308200",
+  appId: "1:902374308200:web:298a6133c1f34c21dd3edd",
+  measurementId: "G-6DVX6SN8CL"
 };
 
 const app = initializeApp(firebaseConfig);
